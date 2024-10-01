0b960a55217e60809302a07c5333d706b9056dba
jian.yang@betacat.onaliyun.com <jian.yang@betacat.onaliyun.com>
2024-10-01 16:04:27 +0800

    main init

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
-package gostudy
+package main
+
+import "net/http"
 
 func main() {
-
+  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("hello, world"))
+  })
+  http.ListenAndServe(":8080", nil)
 }
